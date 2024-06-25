2bdbcd2eb24b73cfb97c0bbf7d0aa81738e64bf7
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2024-06-25 10:23:49 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,3 +1,13 @@
 alert('hello,Osamu!');
 alert(1+1)
 alert8math.sqrt(1+1);
+alert(2+3)
+alert8math.sqrt(2+3);
+alert('2'+'3')
+alert8math.sqrt('2'+'3');
+alert(Number('2')+Number('3')
+alert8math.sqrt(Number('2')+Number('3');
+alert('2'+3)
+alert8math.sqrt('2'+3);
+alert(Number('2')+3
+alert8math.sqrt(Number('2')+3);
